c23743a9c48a901d322caa9d43be9b53c9ebc4e7
chen.guan <chen.guan@shiant.com>
2018-10-14 21:09:35 +0800

    shopping

---
+++ shopping/main.js
@@ -0,0 +1,86 @@
+import Vue from 'vue';
+import App from './app.vue';
+import VueRouter from 'vue-router';
+import Routers from './router.js';
+import product_data from './product.js';
+import Vuex from 'vuex';
+import './style.css';
+
+Vue.use(VueRouter);
+Vue.use(Vuex);
+
+const RouterConfig = {
+  mode: 'history',
+  routes: Routers
+}
+const router = new VueRouter(RouterConfig);
+router.beforeEach((to, from, next)=> {
+  window.document.title = to.meta.title;
+  next();
+})
+
+router.afterEach((to, from, next)=> {
+  window.scrollTo(0, 0);
+})
+
+function getFilterArray(array) {
+  const res = [];
+  const json = {};
+  for(let i=0;i<array.length;i++) {
+    const _self = array[i];
+    if(!json[_self]) {
+      res.push(_self);
+      json[_self] = 1;
+    }
+  }
+  return res;
+}
+
+const store = new Vuex.Store({
+  state: {
+    productList: [],
+    cartList: []
+  },
+  getters: {
+    brands: state => {
+      const brands = state.productList.map(item => item.brand);
+      return getFilterArray(brands);
+    },
+    colors: state => {
+      const colors = state.productList.map(item => item.color);
+      return getFilterArray(colors);
+    }
+  },
+  mutations: {
+    setProductList (state, data) {
+      state.productList = data;
+    },
+    addCart (state, id) {
+      const isAdded = state.cartList.find(item=>item.id === id);
+      if(isAdded) {
+        isAdded.count ++ ;
+      } else {
+        state.cartList.push({
+          id: id,
+          count: 1
+        })
+      }
+    }
+  },
+  actions: {
+    getProductList(context) {
+      setTimeout(() => {
+        context.commit('setProductList', product_data)
+      }, 500);
+    }
+  }
+})
+
+new Vue({
+    el: '#app',
+    router: router,
+    store: store,
+    render: h => {
+        return h(App)
+    }
+});
